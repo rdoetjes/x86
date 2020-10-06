bd36ce08ef2bfaceac10e7386dfc422f9c230e1a
Raymond Doetjes <rdoetjes@phonax.com>
2020-10-06 14:11:15 +0200

    starfield memory map vga

---
+++ CODE/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+  int x, y;
+  int radius = int(sqrt(160 * 160 + 100 * 100));
+  float easyin;
+
+  ofstream file ("starfield.bin", ios::binary);
+
+  for (int angle=0; angle<360; angle+=(360/64)){
+    for (float i=0; i<=1.0; i+=0.05){
+      easyin = i <= 0.5 ? 2 * i * i : i;
+      x = int(radius * easyin *  cos(angle)) + 160;
+      y = int(radius * easyin *  sin(angle)) + 100;
+ 
+      x = x < 0 ? 0 : x;
+      x = x > 319 ? 319 : x;
+
+      y = y < 0 ? 0 : y;
+      y = y > 199 ? 199 : y;
+
+      unsigned int mem = 0xA000 + (320 * x + y);
+      cout << std:: hex << mem << " ";
+      file.write(reinterpret_cast<const char *>(&mem), sizeof(mem));
+    }
+  }
+  return 0;
+}
